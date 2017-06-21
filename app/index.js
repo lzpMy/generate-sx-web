3d29dce6a4f960c3478756c915428a674fa9c6a8
somnus <li_zp@suixingpay.com\lzp3309523>
2017-06-21 16:52:11 +0800

    extends

--- app/index.js
+++ app/index.js
@@ -1,41 +1,79 @@
 var path = require('path');
 var chalk = require('chalk');    //不同颜色的info
 var util = require('util');
-var yeoman = require('yeoman-generator');
+var Generator = require('yeoman-generator');
 var yosay = require('yosay');    //yeoman弹出框
 var path = require('path');
-var Reactpackage = yeoman.Base.extend({
-    info: function() {
-        this.log(chalk.green(
-            'I am going to build your app!'
-        ));
-    },
-    generateBasic: function() {  //按照自己的templates目录自定义
-        this.directory('src', 'src');    //拷贝目录
-        this.directory('builder', 'builder');
-        this.directory('local', 'local');
-        this.directory('public', 'public');
-        this.directory('static', 'static');
-        this.copy('package.json', 'package.json');   //拷贝文件
-        this.copy('index.html', 'index.html');
-        this.copy('README.md', 'README.md');
-        this.copy('postcss.config.js', 'postcss.config.js');
-        this.copy('yarn.lock', 'yarn.lock');
-        this.copy('.babelrc', '.babelrc');
-    },
-    generateClient: function() {
-        this.sourceRoot(path.join(__dirname, 'templates'));
-        this.destinationPath('./');
-    },
-    install: function() {      //安装依赖
-        this.installDependencies({
-            skipInstall: this.options['skip-install']
-        });
-    },
-    end: function() {
-        this.log(yosay(
-            'Your app has been created successfully!'
-        ));
+module.exports =  class extends Generator {
+    constructor(args, opts) {
+        // Calling the super constructor is important so our generator is correctly set up
+        super(args, opts);
+
+        // Next, add your custom code
+        this.option('babel'); // This method adds support for a `--babel` flag
     }
-});
-module.exports = Reactpackage;
+
+    prompting() {
+        return this.prompt([{
+            type    : 'input',
+            name    : 'name',
+            message : 'generator-sx-web',
+        }
+        // , {
+        //     type    : 'confirm',
+        //     name    : 'cool',
+        //     message : 'Would you like to enable the Cool feature?'
+        // }
+        ,{
+            type    : 'input',
+            name    : 'author',
+            message : 'somnus',
+        }
+        ]).then((answers) => {
+            this.appname = answers.name
+            this.author = answers.author
+        });
+
+    }
+
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('public'),
+            this.destinationPath(`${this.appname}/public`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('src'),
+            this.destinationPath(`${this.appname}/src`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('builder'),
+            this.destinationPath(`${this.appname}/builder`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('static'),
+            this.destinationPath(`${this.appname}/static`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath(`${this.appname}/package.json`),
+            {
+                name: this.appname,
+                author: this.author
+            }
+        );
+        this.fs.copyTpl(
+            this.templatePath('index.html'),
+            this.destinationPath(`${this.appname}/index.html`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('postcss.config.js'),
+            this.destinationPath(`${this.appname}/postcss.config.js`)
+        );
+        this.fs.copyTpl(
+            this.templatePath('.babelrc'),
+            this.destinationPath(`${this.appname}/.babelrc`)
+        );
+        this.sourceRoot(`./${this.appname}`);
+        this.npmInstall();
+    }
+};
